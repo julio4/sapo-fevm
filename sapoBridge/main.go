e6ee8284b8f9e3593c35149b8e5c992485e1da20
Julio4 <jules.doumeche@gmail.com>
2023-01-31 13:23:58 +0100

    forked bacalhau poc lilypad

---
+++ sapoBridge/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/julio4/sapo-fevm/sapoBridge/pkg/bridge"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	ctx := log.Logger.WithContext(context.Background())
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	repo, err := bridge.NewSQLiteRepository(ctx, "lilypad.sqlite")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
+
+	workflow := bridge.NewWorkflow(bridge.NewJobRunner(), bridge.TimerContract(), repo)
+
+	err = workflow.Start(ctx)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+	}
+}
