6554569939a0d8228d1ae72a1946051872d768ce
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2023-02-01 20:57:00 +0100

    Basic DataDAO + deploy script

--- datadao/deploy/00_deploy.js
+++ datadao/deploy/00_deploy.js
@@ -9,33 +9,23 @@ const wallet = new ethers.Wallet(private_key, ethers.provider)
 module.exports = async ({ deployments }) => {
     console.log("Wallet Ethereum Address:", wallet.address)
     const chainId = network.config.chainId
-    const tokensToBeMinted = networkConfig[chainId]["tokensToBeMinted"]
 
-    //deploy Simplecoin
-    const SimpleCoin = await ethers.getContractFactory("CoinDAO", wallet)
-    console.log("Deploying Simplecoin...")
-    const simpleCoin = await SimpleCoin.deploy(tokensToBeMinted)
-    await simpleCoin.deployed()
-    console.log("SimpleCoin deployed to:", simpleCoin.address)
+    //deploy MembershipNFT
+    const membershipNFT = await ethers.getContractFactory("MembershipNFT")
+    console.log("Deploying MembershipNFT...")
+    const membershipNFTContract = await membershipNFT.deploy()
+    console.log("a")
+    await membershipNFTContract.deployed()
+    console.log("MembershipNFT deployed to:", membershipNFTContract.address)
 
     //deploy SimpleCIDTracker
-    const DataDAO = await ethers.getContractFactory("SimpleCidTracker", wallet)
-    console.log("Deploying DataDAO...")
-    const dataDAO = await DataDAO.deploy(simpleCoin.address)
-    await dataDAO.deployed()
-    console.log("DataDAO deployed to:", dataDAO.address)
+    var adminsList = ["0x07705e0fFdc102e6144e22261477B4cE46Da350A"]
+    const dataDAOExample = await ethers.getContractFactory("DataDAOExample")
+    console.log("Deploying DataDAOExample...")
+    const dataDAOExampleContract = await dataDAOExample.deploy(adminsList, membershipnftaddress, {
+        gasLimit: 1000000000,
+    })
 
-    // //deploy FilecoinMarketConsumer
-    // const FilecoinMarketConsumer = await ethers.getContractFactory("FilecoinMarketConsumer", wallet)
-    // console.log("Deploying FilecoinMarketConsumer...")
-    // const filecoinMarketConsumer = await FilecoinMarketConsumer.deploy()
-    // await filecoinMarketConsumer.deployed()
-    // console.log("FilecoinMarketConsumer deployed to:", filecoinMarketConsumer.address)
-
-    // //deploy DealRewarder
-    // const DealRewarder = await ethers.getContractFactory("DealRewarder", wallet)
-    // console.log("Deploying DealRewarder...")
-    // const dealRewarder = await DealRewarder.deploy()
-    // await dealRewarder.deployed()
-    // console.log("DealRewarder deployed to:", dealRewarder.address)
+    await dataDAOExampleContract.deployed()
+    console.log("DataDAOExample deployed to:", dataDAOExampleContract.address)
 }
