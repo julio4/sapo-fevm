5d25e5982a603975f67ad2178d2b9d8518130ba4
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2023-02-03 23:32:27 +0100

    upload file to ipfs with lighthouse

--- front/src/pages/testCallCid/index.tsx
+++ front/src/pages/testCallCid/index.tsx
@@ -2,6 +2,7 @@ import Layout from "../../components/layout";
 import DaoNavBar from "../../components/daoNavBar";
 import ZoomCard from "../../components/zoomCard";
 import OcrCard from "../../components/OcrCard";
+import UploadCard from "@/components/uploadFile";
 import { Card, CardBody } from "@chakra-ui/react";
 
 import * as React from "react";
@@ -16,8 +17,8 @@ export default function Home() {
       </Flex>
       <Flex w="50%" direction="column" p={12} rounded={6} alignItems="center">
         <Heading mb={6}>Jobs</Heading>
-        <ZoomCard />
         <OcrCard />
+        <UploadCard />
       </Flex>
     </Layout>
   );
