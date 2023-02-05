da29c96e7b7fffaf310e0cceea7172e342cd3a5c
quintenbons <quintenbons@outlook.com>
2023-02-05 15:09:21 +0100

    CID encoding util

--- sapoBridge/ipfs/test.go
+++ sapoBridge/ipfs/test.go
@@ -6,6 +6,19 @@ import (
 )
 
 func main() {
+	convertToBytes()
+}
+
+func convertToBytes() {
+	cid := "Qmbji4cLwj35C7pfhLWZTte2XX8J6obg6hHVKG6KtXvR5M"
+	cid1, _ := bridge.PackToHex(cid[:32])
+	cid2, _ := bridge.PackToHex(cid[32:])
+
+	log.Print(cid1)
+	log.Print(cid2)
+}
+
+func parseSpecsCid() {
 	jobSpec, err := bridge.ParseSpecs("QmcFV6bjk7JdyyPDCCaSUBigggDP4sq77DtZ2V6s53nnsR")
 
 	if err != nil {
--- sapoBridge/pkg/bridge/byte_encode.go
+++ sapoBridge/pkg/bridge/byte_encode.go
@@ -2,6 +2,7 @@ package bridge
 
 import (
 	"errors"
+	"fmt"
 )
 
 // Unpack a byte array into a string (ignore \x00)
@@ -28,4 +29,20 @@ func Pack(str string) (res [32]byte, err error) {
 	bytes := []byte(str)
 	copy(res[:32], bytes[:])
 	return
+}
+
+func PackToHex(str string) (string, error) {
+	bytes, err := Pack(str)
+
+	if err != nil {
+		return "", err
+	}
+
+	res := "0x"
+
+	for _, b := range bytes {
+		res += fmt.Sprintf("%02x", b)
+	}
+
+	return res, nil
 }
